1b417fa0b87ab9b8580d081e29036b05858119d3
markgordon22 <markgordon97@gmail.com>
2021-05-01 10:06:42 +0000

    ye

--- run.py
+++ run.py
@@ -12,8 +12,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('markgordonsandwiches')
 
-sales = SHEET.worksheet('sales')
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    get sales figures input from user
+    """
+    print("please enter sales data from the last market")
+    print("data should be six numbers with commas")
+    print("Example: 10,20,30,40,50,60,70,80\n")
 
-print(data)
+    data_str = input("enter your data here: ")
+    print(f"the data provided is {data_str}")
+
+get_sales_data()
+
+
+
